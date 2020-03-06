049ce7c86dd045138d706bafb4dcc58ccc46103f
Mark Purcell <mkpurcell@yahoo.com>
2020-03-06 08:47:54 +0000

    Re-enabling the unit test

--- tests/basic.py
+++ tests/basic.py
@@ -30,6 +30,7 @@ import logging
 import time
 import unittest
 import pytest
+import pycloudmessenger.ffl.abstractions as ffl
 import pycloudmessenger.ffl.fflapi as fflapi
 
 
@@ -52,10 +53,12 @@ class MessengerTests(unittest.TestCase):
     def tearDownClass(cls):
         pass
 
-    @unittest.skip("temporarily skipping")
+    #@unittest.skip("temporarily skipping")
     def test_get_tasks(self):
-        context = fflapi.Context.from_credentials_file(self.credentials)
-        user = fflapi.User(context)
+        ffl.Factory.register('cloud', fflapi.Context, fflapi.User, fflapi.Aggregator, fflapi.Participant)
+        context = ffl.Factory.context('cloud', self.credentials)
+
+        user = ffl.Factory.user(context)
         with user:
             result = user.get_tasks()
             for r in result:
