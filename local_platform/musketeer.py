16c46e057cd4deba81be0d8c2ccc583af0d6c213
Ngoc Minh Tran <M.N.Tran@ibm.com>
2020-05-07 11:05:31 +0100

    update musketeer.py
    Signed-off-by: Ngoc Minh Tran <M.N.Tran@ibm.com>

--- local_platform/musketeer.py
+++ local_platform/musketeer.py
@@ -7,6 +7,9 @@ from pycloudmessenger.ffl.abstractions import Notification
 
 HOST = '127.0.0.1'
 PORT = 5000
+NOT_FOUND = 404
+OK = 200
+CONFLICT = 409
 
 app = Flask(__name__)
 log = logging.getLogger('werkzeug')
@@ -37,103 +40,133 @@ def reset():
     participant_list = []
     task_name = []
 
-    return make_response('', 200)
+    return make_response('', OK)
 
 
 @app.route('/create_task', methods=['POST'])
 def create_task():
     from datetime import datetime
 
+    global task_name
+    global definition
+
     message = request.args['message']
     task_name.append({'task_name': request.args['task_name'], 'status': 'CREATED',
                       'added': datetime.now().strftime('%Y-%m-%dT%H:%M:%S')})
     definition.update({'definition': message})
 
-    return make_response('', 200)
+    return make_response('', OK)
 
 
 @app.route('/task_info', methods=['GET'])
 def task_info():
-    return make_response(jsonify({'message': definition}), 200)
+    global definition
+    return make_response(jsonify({'message': definition}), OK)
 
 
 @app.route('/get_tasks', methods=['GET'])
 def get_tasks():
-    return make_response(jsonify({'message': task_name}), 200)
+    global task_name
+    return make_response(jsonify({'message': task_name}), OK)
 
 
 @app.route('/get_joined_tasks', methods=['GET'])
 def get_joined_tasks():
+    global participant_list
+
     user = request.args['message']
     result = task_name if user in participant_list else []
 
-    return make_response(jsonify({'message': result}), 200)
+    return make_response(jsonify({'message': result}), OK)
 
 
 @app.route('/join_task', methods=['POST'])
 def join_task():
-    message = request.args['message']
-    aggregator_queue.append((message, Notification.participant_joined))
+    global aggregator_queue
+    global participant_list
 
-    return make_response('', 200)
+    message = request.args['message']
+
+    if not ((message in participant_list) or ((message, Notification.participant_joined) in aggregator_queue)):
+        aggregator_queue.append((message, Notification.participant_joined))
+        return make_response('', OK)
+    else:
+        return make_response('', CONFLICT)
 
 
 @app.route('/get_participants', methods=['GET'])
 def get_participants():
-    return make_response(jsonify({'message': participant_list}), 200)
+    global participant_list
+    return make_response(jsonify({'message': participant_list}), OK)
 
 
 @app.route('/aggregator_send', methods=['POST'])
 def aggregator_send():
+    global participant_queue
+
     message = request.json['message']
 
     for user in participant_queue.keys():
         participant_queue[user].append(message)
 
-    return make_response('', 200)
+    return make_response('', OK)
 
 
 @app.route('/aggregator_receive', methods=['GET'])
 def aggregator_receive():
-    while True:
-        if len(aggregator_queue) > 0:
-            content = aggregator_queue[0]
+    global aggregator_queue
+    global participant_list
+    global participant_queue
 
-            if content[1] is Notification.participant_joined:
-                participant_list.append(content[0])
-                participant_queue.update({content[0]: []})
-                result = (Notification.participant_joined, content[0])
+    if len(aggregator_queue) > 0:
+        content = aggregator_queue[0]
 
-            elif content[1] is Notification.participant_updated:
-                msg = {'notification': {'type': Notification.participant_updated}}
-                result = {'params': content[0]}
-                result.update(msg)
+        if content[1] is Notification.participant_joined:
+            participant_list.append(content[0])
+            participant_queue.update({content[0]: []})
+            result = (Notification.participant_joined, content[0])
 
-            del aggregator_queue[0]
-            break
+        elif content[1] is Notification.participant_updated:
+            msg = {'notification': {'type': Notification.participant_updated}}
+            result = {'params': content[0]}
+            result.update(msg)
 
-    return make_response(jsonify({'message': result}), 200)
+        final_msg = make_response(jsonify({'message': result}), OK)
+
+        del aggregator_queue[0]
+
+    else:
+        final_msg = make_response('', NOT_FOUND)
+
+    return final_msg
 
 
 @app.route('/participant_send', methods=['POST'])
 def participant_send():
+    global aggregator_queue
+
     message = request.json['message']
     aggregator_queue.append((message, Notification.participant_updated))
 
-    return make_response('', 200)
+    return make_response('', OK)
 
 
 @app.route('/participant_receive', methods=['GET'])
 def participant_receive():
+    global participant_queue
+
     user = request.args['user']
 
-    while True:
-        if len(participant_queue[user]) > 0:
-            result = participant_queue[user][0]
-            del participant_queue[user][0]
-            break
+    if len(participant_queue[user]) > 0:
+        result = participant_queue[user][0]
+        final_msg = make_response(jsonify({'message': result}), OK)
 
-    return make_response(jsonify({'message': result}), 200)
+        del participant_queue[user][0]
+
+    else:
+        final_msg = make_response('', NOT_FOUND)
+
+    return final_msg
 
 
 if __name__ == "__main__":
